6df4428b9ce2f3861e2d57f4b21587864b46a082
Igor Zheleznov <igorzheleznov@gmail.com>
2016-12-28 20:55:58 +0300

    init

---
+++ src/components/tree/consts.js
@@ -0,0 +1,7 @@
+export const NODE_CREATE = 'tree/NODE_CREATE';
+
+export const NODE_EDIT_START = 'tree/NODE_EDIT_START';
+
+export const NODE_EDIT_END = 'tree/NODE_EDIT_END';
+
+export const NODE_UPDATE = 'tree/NODE_UPDATE';
